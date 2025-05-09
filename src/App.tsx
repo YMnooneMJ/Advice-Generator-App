2dfc85fdf96733119834b923adbf27d908b0e9b4
YMnooneJM <demraldo12@gmail.com>
2025-05-09 19:31:46 +0100

    Advice Generator App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { GiDiceFire } from "react-icons/gi";
 import { TiMediaPause } from "react-icons/ti";
 
